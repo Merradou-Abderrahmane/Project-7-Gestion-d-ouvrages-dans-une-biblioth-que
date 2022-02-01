5be4d48a1e605e311dbb0c6d9ff7cdc306bbe0fc
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2022-02-01 13:31:57 +0100

    modify print

--- src/script.js
+++ src/script.js
@@ -151,9 +151,9 @@ function onDelete(buttonReference) {
 
 
 function onPrint() {
-    var tab = document.getElementById('worksTable');
-    var win = window.open();
-    win.document.write(tab.outerHTML);
-    win.document.close();
-    win.print();
+    // var tab = document.getElementById('worksTable');
+    // var win = window.open();
+    // win.document.write(tab.outerHTML);
+    // win.document.close();
+    window.print();
 }
